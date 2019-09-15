4119b4b674590489e74fa54babbe24f045fdb1ce
Zacarias F. Ojeda <zojeda@gmail.com>
2019-09-15 11:31:19 -0300

    mejoras en mapa de calor

--- src/mapaCalor.ts
+++ src/mapaCalor.ts
@@ -1,73 +1,237 @@
 import * as d3 from "d3";
 
-const heatmap_data = require('../data//heatmap_data.csv');
+const heatmap_data = require("../data//heatmap_data.csv");
 
 export interface MapaCalorOpciones {
   padreSelector: string; //elemento contenedor del mapa
+  data: number[][];
+  labelscol: string[];
+  labelsrow: string[];
+  start_color: string;
+  end_color: string;
 }
 
-export function MapaCalor(opciones: MapaCalorOpciones) {
-  var margin = { top: 30, right: 30, bottom: 30, left: 130 },
-    width = 450 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+export function MapaCalor(options: MapaCalorOpciones) {
+  var margin = { top: 50, right: 50, bottom: 150, left: 150 },
+    width = 400,
+    height = 400,
+    data = options.data,
+    container = options.padreSelector,
+    labelscolData = options.labelscol,
+    labelsrowData = options.labelsrow,
+    startColor = options.start_color,
+    endColor = options.end_color;
 
-  // append the svg object to the body of the page
+  var widthLegend = 100;
+
+  if (!data) {
+    throw new Error("Please pass data");
+  }
+
+  // console.log(data);
+
+  if (!Array.isArray(data) || !data.length || !Array.isArray(data[0])) {
+    throw new Error("It should be a 2-D array");
+  }
+
+  var maxValue = d3.max(data, function(layer) {
+    return d3.max(layer, function(d) {
+      return d;
+    });
+  });
+  var minValue = d3.min(data, function(layer) {
+    return d3.min(layer, function(d) {
+      return d;
+    });
+  });
+
+  var numrows = data.length;
+  var numcols = data[0].length;
+
+  // console.log(maxValue, minValue, numcols, numrows);
+
   var svg = d3
-    .select(opciones.padreSelector)
+    .select(container)
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // Labels of row and columns
-  var myGroups = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-  var myVars = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10"];
+  var background = svg
+    .append("rect")
+    .style("stroke", "black")
+    .style("stroke-width", "2px")
+    .attr("width", width)
+    .attr("height", height);
 
-  // Build X scales and axis:
-  var x = d3
-    .scaleBand()
-    .range([0, width])
-    .domain(myGroups)
-    .padding(0.01);
-  svg
+  // console.log(d3.range(numcols));
+
+  var x = d3.scaleBand<number>()
+    .domain(d3.range(numcols))
+    .range([0, width]);
+
+  var y = d3.scaleBand<number>()
+    .domain(d3.range(numrows))
+    .range([0, height]);
+
+  var colorMap = d3.scaleLinear<string>()
+    .domain([minValue, maxValue])
+    .range([startColor, endColor]);
+
+  var row = svg
+    .selectAll(".row")
+    .data(data)
+    .enter()
     .append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .attr("class", "row")
+    .attr("transform", function(d, i) {
+      return "translate(0," + y(i) + ")";
+    });
 
-  // Build X scales and axis:
-  var y = d3
-    .scaleBand()
+  var cell = row
+    .selectAll(".cell")
+    .data(function(d) {
+      return d;
+    })
+    .enter()
+    .append("g")
+    .attr("class", "cell")
+    .attr("transform", function(d, i) {
+      return "translate(" + x(i) + ", 0)";
+    });
+
+  cell
+    .append("rect")
+    .attr("width", x.bandwidth())
+    .attr("height", y.bandwidth())
+    .style("stroke-width", 0);
+
+  cell
+    .append("text")
+    .attr("dy", ".32em")
+    .attr("x", x.bandwidth() / 2)
+    .attr("y", y.bandwidth() / 2)
+    .attr("text-anchor", "middle")
+    .style("fill", function(d, i) {
+      return d >= maxValue / 2 ? "white" : "black";
+    })
+    .text(function(d, i) {
+      return d;
+    });
+
+  row
+    .selectAll(".cell")
+    .data(function(d, i) {
+      return data[i];
+    })
+    .style("fill", colorMap);
+
+  var labels = svg.append("g").attr("class", "labels");
+
+  var columnLabels = labels
+    .selectAll(".column-label")
+    .data(labelscolData)
+    .enter()
+    .append("g")
+    .attr("class", "column-label")
+    .attr("transform", function(d, i) {
+      return "translate(" + x(i) + "," + height + ")";
+    });
+
+  columnLabels
+    .append("line")
+    .style("stroke", "black")
+    .style("stroke-width", "1px")
+    .attr("x1", x.bandwidth() / 2)
+    .attr("x2", x.bandwidth() / 2)
+    .attr("y1", 0)
+    .attr("y2", 5);
+
+  columnLabels
+    .append("text")
+    .attr("x", 0)
+    .attr("y", y.bandwidth() / 2)
+    .attr("dy", ".82em")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-60)")
+    .text(function(d, i) {
+      return d;
+    });
+
+  var rowLabels = labels
+    .selectAll(".row-label")
+    .data(labelsrowData)
+    .enter()
+    .append("g")
+    .attr("class", "row-label")
+    .attr("transform", function(d, i) {
+      return "translate(" + 0 + "," + y(i) + ")";
+    });
+
+  rowLabels
+    .append("line")
+    .style("stroke", "black")
+    .style("stroke-width", "1px")
+    .attr("x1", 0)
+    .attr("x2", -5)
+    .attr("y1", y.bandwidth() / 2)
+    .attr("y2", y.bandwidth() / 2);
+
+  rowLabels
+    .append("text")
+    .attr("x", -8)
+    .attr("y", y.bandwidth() / 2)
+    .attr("dy", ".32em")
+    .attr("text-anchor", "end")
+    .text(function(d, i) {
+      return d;
+    });
+
+  var key = d3
+    .select("#legend")
+    .append("svg")
+    .attr("width", widthLegend)
+    .attr("height", height + margin.top + margin.bottom);
+
+  var legend = key
+    .append("defs")
+    .append("svg:linearGradient")
+    .attr("id", "gradient")
+    .attr("x1", "100%")
+    .attr("y1", "0%")
+    .attr("x2", "100%")
+    .attr("y2", "100%")
+    .attr("spreadMethod", "pad");
+
+  legend
+    .append("stop")
+    .attr("offset", "0%")
+    .attr("stop-color", endColor)
+    .attr("stop-opacity", 1);
+
+  legend
+    .append("stop")
+    .attr("offset", "100%")
+    .attr("stop-color", startColor)
+    .attr("stop-opacity", 1);
+
+  key
+    .append("rect")
+    .attr("width", widthLegend / 2 - 10)
+    .attr("height", height)
+    .style("fill", "url(#gradient)")
+    .attr("transform", "translate(0," + margin.top + ")");
+
+  var yScale = d3.scaleLinear()
     .range([height, 0])
-    .domain(myVars)
-    .padding(0.01);
-  svg.append("g").call(d3.axisLeft(y));
+    .domain([minValue, maxValue]);
 
-  // Build color scale
-  var myColor = d3
-    .scaleLinear<string>()
-    .range(["white", "red"])
-    .domain([1, 100]);
+  var yAxis = d3.axisRight(yScale)
 
-  //Read the data
-  d3.csv(heatmap_data).then(data => {
-    svg
-      .selectAll()
-      .data(data, function(d) {
-        return d.group + ":" + d.variable;
-      })
-      .enter()
-      .append("rect")
-      .attr("x", function(d) {
-        return x(d.group);
-      })
-      .attr("y", function(d) {
-        return y(d.variable);
-      })
-      .attr("width", x.bandwidth())
-      .attr("height", y.bandwidth())
-      .style("fill", function(d: any) {
-        return myColor(d.value);
-      });
-  });
+  key
+    .append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(41," + margin.top + ")")
+    .call(yAxis);
 }
